d2f4ba1705cfbc3532ff3e2375985b5f6973560b
tarjelavik <Tarje.Lavik@ub.uib.no>
2020-09-18 14:37:52 +0200

    Add iiiManifest input plugin

---
+++ template/studio/plugins/sanity-plugin-iiif-manifest-input/src/Mirador.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import mirador from "mirador";
+
+class Mirador extends Component {
+  useEffect() {
+    const { config } = this.props;
+
+    const thisOne = {
+      id: 'arg',
+      windows: {
+        manifestId: config.value
+      },
+      window: {
+        allowClose: false,
+        allowWindowSideBar: false,
+      }
+    }
+    mirador.viewer(thisOne);
+  }
+  render() {
+    return <div id={thisOne.id} />;
+  }
+}
+
+export default Mirador;
---
+++ template/studio/plugins/sanity-plugin-iiif-manifest-input/src/schema.js
@@ -0,0 +1,8 @@
+import IIIFManifestInput from ".";
+
+export default {
+  name: "iiifManifest",
+  type: "string",
+  title: "IIIF Manifest",
+  inputComponent: IIIFManifestInput
+};
