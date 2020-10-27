5be70b5b500cb5c159c560c5b84d691cd1d0392d
tarjelavik <Tarje.Lavik@ub.uib.no>
2020-10-27 18:56:36 +0100

    Update to IIIF presentation v3

--- template/web/pages/api/manifest/[id].js
+++ template/web/pages/api/manifest/[id].js
@@ -5,38 +5,95 @@ async function constructManifest(object) {
   const imageId = object[0].mainRepresentation.iiifImage.url.replace("production", "production/iiif")
 
   let manifest = `{
-    "@context": "http://iiif.io/api/presentation/2/context.json",
+    "@context": "http://iiif.io/api/presentation/3/context.json",
     "id": "https://example.org/iiif/${object[0]._id}/manifest",
     "type": "Manifest",
     "label": { "en": [ "${object[0].label}" ] },
-    "sequences": [
+    "provider": [
       {
-        "@context": "http://iiif.io/api/presentation/2/context.json",
-        "@id": "https://example.org/iiif/${object[0]._id}/seq/s1",
-        "@type": "sc:Sequence",
-        "canvases": [
+        "id": "https://www.uib.no/ub",
+        "type": "Agent",
+        "label": { 
+          "no": [ "Universitetsbiblioteket i Bergen" ],
+          "en": [ "University of Bergen Library" ] 
+        },
+        "homepage": [
           {
-            "@type": "sc:Canvas",
-            "@id": "https://example.org/iiif/${object[0]._id}/canvas/1",
-            "label": "1",
-            "width": 6099,
-            "height": 8599,
-            "images": [
+            "id": "https://www.uib.no/ub",
+            "type": "Text",
+            "label": { 
+              "no": [ "Universitetsbiblioteket i Bergen hjemmeside" ],
+              "en": [ "University of Bergen Library Homepage" ] 
+            },
+            "format": "text/html"
+          }
+        ],
+        "logo": [
+          {
+            "id": "http://marcus.uib.no/img/UiBmerke_grayscale.svg",
+            "type": "Image",
+            "format": "image/svg+xml"
+          }
+        ]
+      }
+    ],
+    "rights": "https://creativecommons.org/licenses/by/4.0/",
+    "requiredStatement": {
+      "label": { 
+        "no": [ "Kreditering" ],
+        "en": [ "Attribution" ] 
+      },
+      "value": { 
+        "no": [ "Tilgjengeliggjort av Universitetsbiblioteket i Bergen" ],
+        "en": [ "Provided by University of Bergen Library" ] 
+      }
+    },
+    "items": [
+      {
+        "id": "https://example.org/iiif/${object[0]._id}/canvas/1",
+        "type": "Canvas",
+        "label": {
+          "none": [ "1" ]
+        },
+        "width": 6099,
+        "height": 8599,
+        "items": [
+          {
+            "id": "https://example.org/iiif/${object[0]._id}/page/1",
+            "type": "AnnotationPage",
+            "items": [
               {
-                "@type": "oa:Annotation",
-                "motivation": "sc:painting",
-                "on": "https://example.org/iiif/${object[0]._id}/canvas/1",
-                "resource": {
-                  "@type": "dctypes:Image",
-                  "@id": "${imageId}",
+                "id": "https://example.org/iiif/${object[0]._id}/annotation/p1",
+                "type": "Annotation",
+                "motivation": "painting",
+                "target": "https://example.org/iiif/${object[0]._id}/canvas/1",
+                "body": {
+                  "id": "${imageId}",
+                  "type": "Image",
+                  "format": "image/jpeg",
                   "service": {
-                    "@context":  "http://iiif.io/api/image/2/context.json",
-                    "@id": "${imageId}",
-                    "profile": "http://iiif.io/api/image/2/level2.json"
+                    "id": "${imageId}",
+                    "type": "ImageService2",
+                    "profile": "level2"
                   }
                 }
               }
             ]
+          }
+        ]
+      }
+    ],
+    "structures": [
+      {
+        "id": "https://example.org/iiif/${object[0]._id}/seq/s1",
+        "type": "Range",
+        "label": {
+          "en": [ "Table of contents" ]
+        },
+        "items": [
+          {
+            "type": "Canvas",
+            "id": "https://example.org/iiif/${object[0]._id}/canvas/1"
           }
         ]
       }
