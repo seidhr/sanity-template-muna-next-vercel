ae41ca5e54df46482572e03f3c0c8747839bfb3c
Tarje Lavik <tarje.lavik@gmail.com>
2020-11-24 16:55:34 +0100

    Fix type mapping error

--- template/studio/plugins/import-tool/src/marcus/apis/mapMediatypes.js
+++ template/studio/plugins/import-tool/src/marcus/apis/mapMediatypes.js
@@ -5,35 +5,21 @@ export const mapMediatypes = (types) => {
 
   types.map((type) => {
     switch (type) {
-      case 'bøker':
+      case 'ubbont:Painting':
         mappedTypes.push({
-          _ref: '9c8240d2-23b6-45f4-8501-bc2723fbf75e',
-          _type: 'reference',
-          _key: nanoid(),
-        })
-        break
-      case 'bilder':
-        mappedTypes.push({
-          _ref: 'c076b082-3c29-47ee-bd0a-9dee05269a47',
+          _ref: 'cc652425-39e2-4c56-9b0f-c570d4f979bf',
           _type: 'reference',
           _key: nanoid(),
         })
         break
       case 'ubbont:Photograph':
         mappedTypes.push({
-          _ref: 'c076b082-3c29-47ee-bd0a-9dee05269a47',
+          _ref: 'c8791a86-bdaa-407f-bcda-3e84c05d0750',
           _type: 'reference',
           _key: nanoid(),
         })
         break
       case 'bibo:Manuscript':
-        mappedTypes.push({
-          _ref: '7f0864c0-82e7-4a05-b7e0-b1dc15f3fe6f',
-          _type: 'reference',
-          _key: nanoid(),
-        })
-        break
-      case 'privatarkivmateriale':
         mappedTypes.push({
           _ref: '7f0864c0-82e7-4a05-b7e0-b1dc15f3fe6f',
           _type: 'reference',
