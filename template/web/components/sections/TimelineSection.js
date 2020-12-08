7fb87ac0b21e06edfe0a53041b8d6632edf3698c
tarjelavik <Tarje.Lavik@ub.uib.no>
2020-12-08 09:10:19 +0100

    Add functions for mapping of internal data to timeline

--- template/web/components/sections/TimelineSection.js
+++ template/web/components/sections/TimelineSection.js
@@ -1,11 +1,155 @@
 import Head from 'next/head'
-import {Container, Box} from '@chakra-ui/react'
+import { Container, Box } from '@chakra-ui/react'
 import PortableTextBlockQuote from '../PortableTextBlockQuote'
 import PortableTextBlock from '../PortableTextBlock'
 
 /*
   NOT WORKING
  */
+const events = item.events.map(function (event, i) {
+  if (event._type == "event") {
+    return {
+      start_date: {
+        year: dayjs(
+          event.timespan[0].beginOfTheBegin
+            ? event.timespan[0].beginOfTheBegin
+            : event.timespan[0].date
+        ).format("YYYY"),
+        day: dayjs(
+          event.timespan[0].beginOfTheBegin
+            ? event.timespan[0].beginOfTheBegin
+            : event.timespan[0].date
+        ).format("DD"),
+        month: dayjs(
+          event.timespan[0].beginOfTheBegin
+            ? event.timespan[0].beginOfTheBegin
+            : event.timespan[0].date
+        ).format("MM")
+      },
+      end_date: {
+        year: dayjs(
+          event.timespan[0].endOfTheEnd
+            ? event.timespan[0].endOfTheEnd
+            : event.timespan[0].date
+        ).format("YYYY"),
+        day: dayjs(
+          event.timespan[0].endOfTheEnd
+            ? event.timespan[0].endOfTheEnd
+            : event.timespan[0].date
+        ).format("DD"),
+        month: dayjs(
+          event.timespan[0].endOfTheEnd
+            ? event.timespan[0].endOfTheEnd
+            : event.timespan[0].date
+        ).format("MM")
+      },
+      media: event.media
+        ? Object.assign({}, event.media, {
+          caption: event.media.caption,
+          credit: event.media.credit
+        })
+        : "",
+      text: Object.assign(
+        {},
+        {
+          headline: event.label.nor,
+          text: event.description
+            ? blocksToHtml({
+              blocks: event.description.nor.filter(({ _key = "" }) => _key)
+            })
+            : ""
+        }
+      )
+    };
+  }
+  if (event._type == "timelineSlide") {
+    return {
+      start_date: {
+        year: dayjs(
+          event.timespan[0].beginOfTheBegin
+            ? event.timespan[0].beginOfTheBegin
+            : event.timespan[0].date
+        ).format("YYYY"),
+        day: dayjs(
+          event.timespan[0].beginOfTheBegin
+            ? event.timespan[0].beginOfTheBegin
+            : event.timespan[0].date
+        ).format("DD"),
+        month: dayjs(
+          event.timespan[0].beginOfTheBegin
+            ? event.timespan[0].beginOfTheBegin
+            : event.timespan[0].date
+        ).format("MM")
+      },
+      end_date: {
+        year: dayjs(
+          event.timespan[0].endOfTheEnd
+            ? event.timespan[0].endOfTheEnd
+            : event.timespan[0].date
+        ).format("YYYY"),
+        day: dayjs(
+          event.timespan[0].endOfTheEnd
+            ? event.timespan[0].endOfTheEnd
+            : event.timespan[0].date
+        ).format("DD"),
+        month: dayjs(
+          event.timespan[0].endOfTheEnd
+            ? event.timespan[0].endOfTheEnd
+            : event.timespan[0].date
+        ).format("MM")
+      },
+      media: event.media
+        ? Object.assign({}, event.media, {
+          url: event.media.url ? event.media.url : null,
+          caption: event.media.caption ? event.media.caption : null,
+          credit: event.media.credit ? event.media.credit : null
+        })
+        : "",
+      text: Object.assign(
+        {},
+        {
+          headline: event.headline.nor,
+          text: event.text.nor
+            ? blocksToHtml({
+              blocks: event.text.nor.filter(({ _key = "" }) => _key)
+            })
+            : ""
+        }
+      )
+    };
+  }
+});
+const tl = Object.assign({}, item, {
+  title: {
+    text: {
+      headline: item.headline.nor,
+      text: item.text.nor
+        ? blocksToHtml({
+          blocks: item.text.nor.filter(({ _key = "" }) => _key)
+        })
+        : ""
+    },
+    /* background: Object.assign({}, title.background, {
+        color: title.background.color.hex ? title.background.color.hex : '#dddddd'
+      }), */
+    media: {
+      url: item.media[0].url ? item.media[0].url : null,
+      caption: item.media[0].caption
+        ? blocksToHtml({
+          blocks: item.media[0].caption.filter(({ _key = "" }) => _key)
+        })
+        : "",
+      credit: item.media[0].credit
+        ? blocksToHtml({
+          blocks: item.media[0].credit.filter(({ _key = "" }) => _key)
+        })
+        : ""
+    }
+  },
+  events
+});
+
+
 export default function Quote(props) {
   return (
     <Container maxW="4xl" marginTop="10">
@@ -24,7 +168,7 @@ export default function Quote(props) {
           }}
         />
       </Head>
-      <div id="timeline-embed" style={{width: '100%', height: '500px'}}></div>
+      <div id="timeline-embed" style={{ width: '100%', height: '500px' }}></div>
       <Box>
         <Box size="md">
           <PortableTextBlockQuote fontSize="2xl" blocks={props.content} />
